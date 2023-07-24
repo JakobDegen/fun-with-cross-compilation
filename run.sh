d58ceb7f3813d69fff820d70e0836ec1b153b0ea
Jakob Degen <jakob.e.degen@gmail.com>
2023-07-23 18:48:05 -0700

    Make channel configurable

--- run.sh
+++ run.sh
@@ -5,20 +5,21 @@
 
 set -e
 
+export CHANNEL=nightly
 export T32=i686-unknown-linux-gnu
 export T64=x86_64-unknown-linux-gnu
 
 rm -r build || true ; mkdir build
 
-rustc +nightly-$T32 --target $T64 --edition 2021 -C metadata=p --crate-name p p.rs -o build/libp.rlib
-rustc +nightly-$T32 --target $T64 --edition 2021 -C metadata=l --crate-name l l.rs -o build/libl.rlib --extern p=build/libp.rlib
+rustc +$CHANNEL-$T32 --target $T64 --edition 2021 -C metadata=p --crate-name p p.rs -o build/libp.rlib
+rustc +$CHANNEL-$T32 --target $T64 --edition 2021 -C metadata=l --crate-name l l.rs -o build/libl.rlib --extern p=build/libp.rlib
 
 # Copy for debugging purposes
 cp -r build buildb
 
 rm build/libp.rlib
-rustc +nightly-$T64 --target $T64 --edition 2021 -C metadata=p --crate-name p p.rs -o build/libp.rlib
-rustc +nightly-$T64 --target $T64 --edition 2021 -C metadata=b --crate-name b b.rs -o build/bin --extern l=build/libl.rlib -L dependency=build
+rustc +$CHANNEL-$T64 --target $T64 --edition 2021 -C metadata=p --crate-name p p.rs -o build/libp.rlib
+rustc +$CHANNEL-$T64 --target $T64 --edition 2021 -C metadata=b --crate-name b b.rs -o build/bin --extern l=build/libl.rlib -L dependency=build
 
 chmod u+x build/bin
 ./build/bin
