dbca1df6d779880b004e7cebd459e56385c0064f
Jakob Degen <jakob.e.degen@gmail.com>
2023-07-23 17:09:56 -0700

    Fix example

--- run.sh
+++ run.sh
@@ -11,8 +11,11 @@ export T64=x86_64-unknown-linux-gnu
 rm -r build1 || true ; mkdir build1
 rm -r build2 || true ; mkdir build2
 
-rustc +stable-$T32 --target $T32 --edition 2021 -C metadata=a --crate-name p32 p.rs -o build1/libp32.so 
-rustc +stable-$T32 --target $T64 --edition 2021 -C metadata=b --crate-name l l.rs -o build1/libl.rlib --extern p=build1/libp32.so
+rustc +stable-$T32 --target $T32 --edition 2021 -C metadata=p --crate-name p p.rs -o build1/libp32.so 
+rustc +stable-$T32 --target $T64 --edition 2021 -C metadata=l --crate-name l l.rs -o build1/libl.rlib --extern p=build1/libp32.so
 
-rustc +stable-$T64 --target $T64 --edition 2021 -C metadata=c --crate-name p64 p.rs -o build2/libp64.so
-rustc +stable-$T64 --target $T64 --edition 2021 -C metadata=d --crate-name b b.rs -o build1/bin --extern l=build1/libl.rlib -L dependency=build2
+rustc +stable-$T64 --target $T64 --edition 2021 -C metadata=p --crate-name p p.rs -o build2/libp64.so
+rustc +stable-$T64 --target $T64 --edition 2021 -C metadata=b --crate-name b b.rs -o build1/bin --extern l=build1/libl.rlib -L dependency=build2
+
+chmod u+x build1/bin
+./build1/bin
