4326321268a92a1cb90a1485ca09177ea9f2999e
Jakob Degen <jakob.e.degen@gmail.com>
2023-07-23 17:05:46 -0700

    Don't fail if dirs are missing

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ set -e
 export T32=i686-unknown-linux-gnu
 export T64=x86_64-unknown-linux-gnu
 
-rm -r build1 ; mkdir build1
-rm -r build2 ; mkdir build2
+rm -r build1 || true ; mkdir build1
+rm -r build2 || true ; mkdir build2
 
 rustc +stable-$T32 --target $T32 --edition 2021 -C metadata=a --crate-name p32 p.rs -o build1/libp32.so 
 rustc +stable-$T32 --target $T64 --edition 2021 -C metadata=b --crate-name l l.rs -o build1/libl.rlib --extern p=build1/libp32.so
