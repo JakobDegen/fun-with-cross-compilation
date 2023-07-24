e893378a18a3bbbd05e5b3038235089edba038bc
Jakob Degen <jakob.e.degen@gmail.com>
2023-07-23 18:39:21 -0700

    Switch to nightly

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# $ rustup toolchain install stable-i686-unknown-linux-gnu
-# $ rustup target add x86_64-unknown-linux-gnu --toolchain stable-i686-unknown-linux-gnu
+# $ rustup toolchain install nightly-i686-unknown-linux-gnu
+# $ rustup target add x86_64-unknown-linux-gnu --toolchain nightly-i686-unknown-linux-gnu
 
 set -e
 
@@ -10,15 +10,15 @@ export T64=x86_64-unknown-linux-gnu
 
 rm -r build || true ; mkdir build
 
-rustc +stable-$T32 --target $T64 --edition 2021 -C metadata=p --crate-name p p.rs -o build/libp.rlib
-rustc +stable-$T32 --target $T64 --edition 2021 -C metadata=l --crate-name l l.rs -o build/libl.rlib --extern p=build/libp.rlib
+rustc +nightly-$T32 --target $T64 --edition 2021 -C metadata=p --crate-name p p.rs -o build/libp.rlib
+rustc +nightly-$T32 --target $T64 --edition 2021 -C metadata=l --crate-name l l.rs -o build/libl.rlib --extern p=build/libp.rlib
 
 # Copy for debugging purposes
 cp -r build buildb
 
 rm build/libp.rlib
-rustc +stable-$T64 --target $T64 --edition 2021 -C metadata=p --crate-name p p.rs -o build/libp.rlib
-rustc +stable-$T64 --target $T64 --edition 2021 -C metadata=b --crate-name b b.rs -o build/bin --extern l=build/libl.rlib -L dependency=build
+rustc +nightly-$T64 --target $T64 --edition 2021 -C metadata=p --crate-name p p.rs -o build/libp.rlib
+rustc +nightly-$T64 --target $T64 --edition 2021 -C metadata=b --crate-name b b.rs -o build/bin --extern l=build/libl.rlib -L dependency=build
 
 chmod u+x build/bin
 ./build/bin
